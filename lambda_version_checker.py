e1bf6a885d744e8cc3ab44f84dd92ed664ced831
sultan11998 <81705818+sultan11998@users.noreply.github.com>
2021-07-07 10:25:19 -0400

    Update lambda_version_checker.py

--- lambda_version_checker.py
+++ lambda_version_checker.py
@@ -1,43 +1,45 @@
 import boto3
 import os
-#uses CLI command
-stream = os.popen("aws lambda list-functions --region us-east-1 --query Functions[].FunctionName --output text")
-my_dict = stream.read()
-list = my_dict.split();
-output_list = []
+def lambda_handler(event,context):
+    #uses CLI command
+    stream = os.popen("aws lambda list-functions --region us-east-1 --query Functions[].FunctionName --output text")
+    my_dict = stream.read()
+    list = my_dict.split();
+    output_list = []
+    
+    print('Processing...')
+    client = boto3.client('lambda', region_name='us-east-1')
+    for x in list:
+    
+        response = client.get_function(
+            FunctionName=x)
+    
+        try:
+            for i in (response['Tags']):
+                if response['Tags']['TEST.Code'] == "NOTUPTODATE":
+                    for q in (response['Configuration']):
+                        if response['Configuration']['Runtime'] == 'python2.7':
+                            print(response['Configuration']['FunctionName'] + " is in " + response['Configuration'][
+                                'Runtime'])
+                            output = response['Configuration']['FunctionName']
+                            output_list.append(output)
+    
+                            break;
+    
+                break;
+    
+        except:
+            pass
+    
+    sns_client = boto3.client('sns')
+    str1 = ', '.join(output_list)
+    
+    response = sns_client.publish(
+        Message='The following lambdas are out of date: ' + str1,
+        TopicArn="#########################", #ARN value can be obtained when creating SNS topic
+        Subject='Out of date lambda functions'
+    
+    )
+    
+    return('Success')
 
-print('Processing...')
-client = boto3.client('lambda', region_name='us-east-1')
-for x in list:
-
-    response = client.get_function(
-        FunctionName=x)
-
-    try:
-        for i in (response['Tags']):
-            if response['Tags']['TEST.Code'] == "NOTUPTODATE":
-                for q in (response['Configuration']):
-                    if response['Configuration']['Runtime'] == 'python2.7':
-                        print(response['Configuration']['FunctionName'] + " is in " + response['Configuration'][
-                            'Runtime'])
-                        output = response['Configuration']['FunctionName']
-                        output_list.append(output)
-
-                        break;
-
-            break;
-
-    except:
-        pass
-
-sns_client = boto3.client('sns')
-str1 = ', '.join(output_list)
-
-response = sns_client.publish(
-    Message='The following lambdas are out of date: ' + str1,
-    TopicArn="#########################", #ARN value can be obtained when creating SNS topic
-    Subject='Out of date lambda functions'
-
-)
-
-print('Success')
